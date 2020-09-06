37c0f7f16a249bfceedb8acd5613df51baa26380
Nick Underhill <m.podopryhora@gmail.com>
2020-09-06 15:18:46 +0300

    input validation added

--- src/main/java/nick/underhill/App.java
+++ src/main/java/nick/underhill/App.java
@@ -3,32 +3,34 @@ package nick.underhill;
 import java.util.Scanner;
 
 public class App {
-    final String ERROR_MESSAGE = "Error";
+    static final String MESSAGE_RADIX_INPUT = "Enter the base of input number (between 1 and 36):";
+    static final String MESSAGE_RADIX_OUTPUT = "Enter the base of the output number (between 1 and 36):";
+    static final String MESSAGE_NUMBER_INPUT = "Enter a number to convert (fractions allowed):";
+    static final String MESSAGE_CONVERSION_RESULT = "The result of converting %s from radix %s to radix %s: %s";
+    static final String ERROR_MESSAGE_NUMBER_FORMAT = "Enter a number to convert (fractions allowed):";
+    static final String REGEX_RADIX = "^([1-9]|[12]\\d|3[0-6])$";
+    static final String REGEX_NUMBER = "^[+-]?([a-zA-Z0-9]*[.])?[a-zA-Z0-9]+$";
 
     public static void main(String[] args) {
-        String numberInput = "";
+        String numberInput;
+        String fromRadix;
+        String toRadix;
         String result;
-
-        int fromRadix = 1;
-        int toRadix = 1;
-
         Scanner sc = new Scanner(System.in);
 
+        fromRadix = ConversionUtil.validateInputRegEx(sc, REGEX_RADIX, MESSAGE_RADIX_INPUT);
 
-        // TODO: 04.09.2020 Input validation
-        System.out.println("Enter the base of input number (between 1 and 36):");
-        fromRadix = sc.nextInt();
+        numberInput = ConversionUtil.validateInputRegEx(sc, REGEX_NUMBER, MESSAGE_NUMBER_INPUT);
+        if (!ConversionUtil.isConvertible(numberInput, fromRadix)) {
+            System.out.print(ERROR_MESSAGE_NUMBER_FORMAT);
+            numberInput = ConversionUtil.validateInputRegEx(sc, REGEX_NUMBER, MESSAGE_NUMBER_INPUT);
+        }
 
-        System.out.println("Enter a number to convert (1 - z, fractions allowed):");
-        numberInput = sc.next();
-
-        System.out.println("Enter the base of output number (between 1 and 36):");
-        toRadix = sc.nextInt();
+        toRadix = ConversionUtil.validateInputRegEx(sc, REGEX_RADIX, MESSAGE_RADIX_OUTPUT);
 
         result = ConversionUtil.convert(numberInput, fromRadix, toRadix);
         System.out.println(String.format(
-                "The result of converting %s from radix %s to radix %s :",
-                numberInput, fromRadix, toRadix));
-        System.out.println(result);
+                MESSAGE_CONVERSION_RESULT,
+                numberInput, fromRadix, toRadix, result));
     }
 }
--- src/main/java/nick/underhill/ConversionUtil.java
+++ src/main/java/nick/underhill/ConversionUtil.java
@@ -1,22 +1,54 @@
 package nick.underhill;
 
+import java.util.Scanner;
+
 public class ConversionUtil {
 
-    public static String convert (String inputNumberString, int inputRadix, int outputRadix) {
+    private static final int PRECISION = 5;
+
+    private ConversionUtil() {
+    }
+
+    /**
+     * Chooses number conversion logic depending on inputNumberString format.
+     *
+     * @param inputNumberString - String value of number to convert
+     * @param inputRadix        - String value of base of inputted number
+     * @param outputRadix       - String value of desired base
+     * @return String number in given outputRadix
+     */
+    public static String convert(String inputNumberString, String inputRadix, String outputRadix) {
         String result;
         if (inputNumberString.contains(".")) {
-            String[] parts;
-            parts = inputNumberString.split("\\.");
+            String[] parts = inputNumberString.split("\\.");
             String integerPart = parts[0];
             String fractionalPart = parts[1];
-            result = convertInteger(integerPart, inputRadix, outputRadix) + "." +
-                    ConversionUtil.convertFraction(fractionalPart, inputRadix, outputRadix);
+            result = convertInteger(
+                    integerPart,
+                    Integer.parseInt(inputRadix),
+                    Integer.parseInt(outputRadix))
+                    + "."
+                    + convertFraction(
+                            fractionalPart,
+                    Integer.parseInt(inputRadix),
+                    Integer.parseInt(outputRadix));
         } else {
-            result = ConversionUtil.convertInteger(inputNumberString, inputRadix, outputRadix);
+            result = ConversionUtil.convertInteger(
+                    inputNumberString,
+                    Integer.parseInt(inputRadix),
+                    Integer.parseInt(outputRadix));
         }
         return result;
     }
 
+    /**
+     * Converts decimal number from inputRadix base to outputRadix base.
+     *
+     * @param inputNumberString - number to convert
+     * @param inputRadix        - base of inputted number
+     * @param outputRadix       - desired base
+     * @return String number in given outputRadix
+     */
     public static String convertInteger(String inputNumberString, int inputRadix, int outputRadix) {
         int decimalInput;
         if (inputRadix == 1) {
@@ -36,10 +68,19 @@ public class ConversionUtil {
         }
     }
 
+    /**
+     * Converts fractional part of a number from inputRadix base to outputRadix base.
+     *
+     * @param inputNumberString - number to convert
+     * @param inputRadix        - base of inputted number
+     * @param outputRadix       - desired base
+     * @return String number in given outputRadix
+     */
     static String convertFraction(String inputNumberString, int inputRadix, int outputRadix) {
         double decimalResult = 0;
         StringBuilder result = new StringBuilder();
 
+        // Convert to decimal if the number is not in decimal base
         if (inputRadix != 10) {
             char[] addends = inputNumberString.toCharArray();
             for (int i = 0; i < addends.length; i++) {
@@ -50,14 +91,44 @@ public class ConversionUtil {
             decimalResult = Double.parseDouble("0." + inputNumberString);
         }
 
-        int integerPart;
-        for (int i = 0; i < 5; i++) {
+        //Conversion algorithm to transform number to a given base with PRECISION digits after the floating-point
+        for (int i = 0; i < PRECISION; i++) {
             decimalResult = decimalResult * outputRadix; // Multiply fractional part by radix
-            integerPart = (int) decimalResult; // Extract integer part from result
+            int integerPart = (int) decimalResult; // Extract integer part from result
             result.append(Character.forDigit(integerPart, outputRadix)); // Convert integer part to desired radix and append to result String
             decimalResult -= integerPart; // Subtract integer part from result
         }
-
         return result.toString();
+    }
+
+    /**
+     * Validates input using given regular expression.
+     * @param scanner - Scanner object;
+     * @param regex - regular expression which must be satisfied in order to pass input value;
+     * @param msg - text message shown before the input;
+     * @return String
+     */
+    static String validateInputRegEx(Scanner scanner, String regex, String msg) {
+        String result;
+        do {
+            System.out.println(msg);
+            result = scanner.next();
+        } while (!result.matches(regex));
+        return result;
+    }
+
+    /**
+     * Returns true if the given number is in the given radix scope, false if not.
+     * @param number - number to check;
+     * @param radix - extimated radix;
+     * @return boolean
+     */
+    public static boolean isConvertible(String number, String radix) {
+        try {
+            Integer.parseInt(number, Integer.parseInt(radix));
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
